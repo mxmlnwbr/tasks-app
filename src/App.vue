d2e81eedddd0dda30962f70eec0d2c29e76c420c
mxmlnwbr <maximilian.weber@bluewin.ch>
2025-06-11 11:34:56 +0200

    add: form input and reactivity

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,23 @@
 <script lang="ts" setup>
 import { ref } from "vue";
 
-const message = ref("Hello World");
+const message = ref("Tasks App");
+const newTask = ref("");
 </script>
 
 <template>
   <main>
     <h1>{{ message }}</h1>
+    <form>
+      <label
+        >New Task
+        <input v-model="newTask" name="newTask" />
+      </label>
+      <div class="button-container">
+        <button>Add</button>
+      </div>
+    </form>
+    <h3>{{ newTask }}</h3>
   </main>
 </template>
 
@@ -14,5 +25,10 @@ const message = ref("Hello World");
 main {
   max-width: 800px;
   margin: 1rem auto;
+}
+
+.button-container {
+  display: flex;
+  justify-content: end;
 }
 </style>
